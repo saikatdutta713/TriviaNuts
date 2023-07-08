10f3b0750057d7b58879176f5bc2df5a3133ba42
PritamBag <pritam02b@gmail.com>
2023-07-08 16:58:33 +0530

    header designed in master layout

---
+++ resources/views/layout/master.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Home Page</title>
+    <link rel="stylesheet" href="css/app.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
+
+    
+</head>
+<body>
+        <div class="header_div">
+            <div class="logo">
+                Logo
+            </div>
+            <div class="nav_bar">
+                <div class="home_page">
+                    Home
+                </div>
+                <div class="community_page">
+                    Community
+                </div>
+                <div class="trends_page">
+                    Trends
+                </div>
+                <div class="category_page">
+                    Category
+                </div>
+            </div>
+            <div class="login">
+                Login
+            </div>
+        </div>
+</body>
+</html>
