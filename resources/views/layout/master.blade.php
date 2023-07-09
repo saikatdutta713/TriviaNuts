60241447bc60941da5f1dc93dd5bc0b9a342bc78
PritamBag <pritam02b@gmail.com>
2023-07-09 19:10:39 +0530

    footer added in master layout, fontawesome file add in public/css

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -6,26 +6,25 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Home Page</title>
     <link rel="stylesheet" href="css/app.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
-
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
     
 </head>
 <body>
-        <div class="header_div">
+        <div class="header">
             <div class="logo">
                 Logo
             </div>
-            <div class="nav_bar">
-                <div class="home_page">
+            <div class="navbar">
+                <div class="navbutton">
                     Home
                 </div>
-                <div class="community_page">
+                <div class="navbutton">
                     Community
                 </div>
-                <div class="trends_page">
+                <div class="navbutton">
                     Trends
                 </div>
-                <div class="category_page">
+                <div class="navbutton">
                     Category
                 </div>
             </div>
@@ -33,5 +32,52 @@
                 Login
             </div>
         </div>
+
+{{-- 
+    <article>
+        @yield('content')
+    </article> --}}
+
+
+
+        <div class="footer">
+            <div class="row1">
+                <div class="company_tab">
+                    <div class="details" >
+                        Contact
+                    </div>
+                    <div class="details" >
+                        About
+                    </div>
+                    <div class="details" >
+                        Privacy Policy
+                    </div>
+                    <div class="details">
+                        Terms and Conditions
+                    </div>
+                </div>
+                <div class="social_tab">
+                    <div class="media">
+                        <i class="fa-brands fa-facebook"></i>                   </div>
+                    <div class="media">
+                        <i class="fa-brands fa-twitter"></i>
+                    </div>
+                    <div class="media">
+                        <i class="fa-brands fa-instagram"></i>
+                    </div>
+                    <div class="media">
+                        <i class="fa-brands fa-linkedin"></i>
+                    </div>
+                </div>
+            </div>
+            <div class="row2">
+                <div class="copyright">
+                    Copyright
+                </div>
+            </div>
+        </div>
+
+
+
 </body>
 </html>
