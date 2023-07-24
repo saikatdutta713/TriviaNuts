1cd20bbb551cbc5746eb186238349870722f6c63
PritamBag <pritam02b@gmail.com>
2023-07-24 19:56:11 +0530

    Category Page design completed except rules-regulation window

--- resources/views/category.blade.php
+++ resources/views/category.blade.php
@@ -3,9 +3,9 @@
     @section('content')
         <div class="scategory">
             <div class="punch_line">
-                
+                <p>
                     Your limitations are just opportunities to push beyond what you thought was possible. 
-                
+                </p>
             </div>    
             <div class="sdisplay">
                 <div class="subject">
@@ -23,7 +23,158 @@
                     </div>
                 </div>
                 <div class="metarial">
-                    Content
+                    <div class="metarial1">
+                        <i class="fa-solid fa-diamond-turn-right fa-flip-horizontal fa-3x"></i>
+                        <p>Choose any one subject from left to start your Quiz</p>
+                    </div>
+                    <div class="metarial2">
+                        <div class="quizmessage">
+                            <p>Welcome @user play now!!</p>
+                        </div>
+                        <div class="quizno">
+                            <div class="quizrow1">
+                                <div class="quizno1">
+                                    <div class="bhover">
+                                        No. 1
+                                    </div>
+                                    <div class="ahover">
+                                        <div class="starts">
+                                            <i class="fa-solid fa-lock fa-lg"></i>
+                                        </div>
+                                        <div class="startb">
+                                            <i class="fa-solid fa-circle-play fa-2xl"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="quizno1">
+                                    <div class="bhover">
+                                        No. 2
+                                    </div>
+                                    <div class="ahover">
+                                        <div class="starts">
+                                            <i class="fa-solid fa-lock fa-lg"></i>
+                                        </div>
+                                        <div class="startb">
+                                            <i class="fa-solid fa-circle-play fa-2xl"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="quizno1">
+                                    <div class="bhover">
+                                        No. 3
+                                    </div>
+                                    <div class="ahover">
+                                        <div class="starts">
+                                            <i class="fa-solid fa-lock fa-lg"></i>
+                                        </div>
+                                        <div class="startb">
+                                            <i class="fa-solid fa-circle-play fa-2xl"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="quizno1">
+                                    <div class="bhover">
+                                        No. 4
+                                    </div>
+                                    <div class="ahover">
+                                        <div class="starts">
+                                            <i class="fa-solid fa-lock fa-lg"></i>
+                                        </div>
+                                        <div class="startb">
+                                            <i class="fa-solid fa-circle-play fa-2xl"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="quizno1">
+                                    <div class="bhover">
+                                        No. 5
+                                    </div>
+                                    <div class="ahover">
+                                        <div class="starts">
+                                            <i class="fa-solid fa-lock fa-lg"></i>
+                                        </div>
+                                        <div class="startb">
+                                            <i class="fa-solid fa-circle-play fa-2xl"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="quizrow2">
+                                <div class="quizno1">
+                                    <div class="bhover">
+                                        No. 6
+                                    </div>
+                                    <div class="ahover">
+                                        <div class="starts">
+                                            <i class="fa-solid fa-lock fa-lg"></i>
+                                        </div>
+                                        <div class="startb">
+                                            <i class="fa-solid fa-circle-play fa-2xl"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="quizno1">
+                                    <div class="bhover">
+                                        No. 7
+                                    </div>
+                                    <div class="ahover">
+                                        <div class="starts">
+                                            <i class="fa-solid fa-lock fa-lg"></i>
+                                        </div>
+                                        <div class="startb">
+                                            <i class="fa-solid fa-circle-play fa-2xl"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="quizno1">
+                                    <div class="bhover">
+                                        No. 8
+                                    </div>
+                                    <div class="ahover">
+                                        <div class="starts">
+                                            <i class="fa-solid fa-lock fa-lg"></i>
+                                        </div>
+                                        <div class="startb">
+                                            <i class="fa-solid fa-circle-play fa-2xl"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="quizno1">
+                                    <div class="bhover">
+                                        No. 9
+                                    </div>
+                                    <div class="ahover">
+                                        <div class="starts">
+                                            <i class="fa-solid fa-lock fa-lg"></i>
+                                        </div>
+                                        <div class="startb">
+                                            <i class="fa-solid fa-circle-play fa-2xl"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="quizno1">
+                                    <div class="bhover">
+                                        No. 10
+                                    </div>
+                                    <div class="ahover">
+                                        <div class="starts">
+                                            <i class="fa-solid fa-lock fa-lg"></i>
+                                        </div>
+                                        <div class="startb">
+                                            <i class="fa-solid fa-circle-play fa-2xl"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="quizno2">
+                                    <i class="fa-solid fa-angles-right fa-lg"></i>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="quizstartb">
+                            <i class="fa-solid fa-circle-play fa-xl"></i>
+                            Start
+                        </div>
+                    </div>
                 </div>
             </div>
             <div class="fquestions">
@@ -43,42 +194,66 @@
                 <div class="rbadge">
                     <div class="badgefirst">
                         <div class="badge1">
-                            <i class="fa-solid fa-heart fa-2xl" ></i>
+                            <i class="fa-solid fa-heart fa-2xl" id="badgeicon"></i>
+                            <h1>Badge1</h1>
+                            <p>100$</p>
                         </div>
                         <div class="badge1">
-                            <i class="fa-solid fa-hat-cowboy fa-2xl" ></i>
+                            <i class="fa-solid fa-hat-cowboy fa-2xl" id="badgeicon"></i>
+                            <h1>Badge2</h1>
+                            <p>200$</p>
                         </div>
                         <div class="badge1">
-                            <i class="fa-solid fa-spider fa-2xl"></i>
+                            <i class="fa-solid fa-spider fa-2xl" id="badgeicon"></i>
+                            <h1>Badge3</h1>
+                            <p>300$</p>
                         </div>
                         <div class="badge1">
-                            <i class="fa-solid fa-hippo fa-2xl" ></i>
+                            <i class="fa-solid fa-hippo fa-2xl" id="badgeicon"></i>
+                            <h1>Badge4</h1>
+                            <p>400$</p>
                         </div>
                         <div class="badge1">
-                            <i class="fa-solid fa-shield-cat fa-2xl" ></i>
+                            <i class="fa-solid fa-shield-cat fa-2xl" id="badgeicon"></i>
+                            <h1>Badge5</h1>
+                            <p>500$</p>
                         </div>
                         <div class="badge1">
-                            <i class="fa-solid fa-shield-dog fa-2xl" fa-2xl></i>
+                            <i class="fa-solid fa-shield-dog fa-2xl fa-2xl" id="badgeicon"></i>
+                            <h1>Badge6</h1>
+                            <p>600$</p>
                         </div>
                     </div>
                     <div class="badgesecond">
                         <div class="badge1">
-                            <i class="fa-solid fa-paw fa-2xl"></i>
+                            <i class="fa-solid fa-paw fa-2xl" id="badgeicon"></i>
+                            <h1>Badge7</h1>
+                            <p>700$</p>
                         </div>
                         <div class="badge1">
-                            <i  class="fa-solid fa-shield-virus fa-2xl"></i>
+                            <i  class="fa-solid fa-shield-virus fa-2xl" id="badgeicon"></i>
+                            <h1>Badge8</h1>
+                            <p>800$</p>
                         </div>
                         <div class="badge1">
-                            <i class="fa-solid fa-shield-heart fa-2xl"></i>
+                            <i class="fa-solid fa-shield-heart fa-2xl" id="badgeicon"></i>
+                            <h1>Badge9</h1>
+                            <p>900$</p>
                         </div>
                         <div class="badge1">
-                            <i class="fa-sharp fa-solid fa-star-half-stroke fa-2xl" ></i>
+                            <i class="fa-sharp fa-solid fa-star-half-stroke fa-2xl" id="badgeicon"></i>
+                            <h1>Badge10</h1>
+                            <p>1000$</p>
                         </div>
                         <div class="badge1">
-                            <i class="fa-brands fa-wolf-pack-battalion fa-2xl" ></i>
+                            <i class="fa-brands fa-wolf-pack-battalion fa-2xl" id="badgeicon"></i>
+                            <h1>Badge11</h1>
+                            <p>1100$</p>
                         </div>
                         <div class="badge1">
-                            <i class="fa-solid fa-gem fa-2xl" ></i>
+                            <i class="fa-solid fa-gem fa-2xl" id="badgeicon"></i>
+                            <h1>Badge12</h1>
+                            <p>1200$</p>
                         </div>
                     </div>
                 </div>
