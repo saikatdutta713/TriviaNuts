911eb06a0102af61179df4209ae44d9c49db1a3b
PritamBag <pritam02b@gmail.com>
2023-09-08 19:43:32 +0530

    leaderboard added

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -52,17 +52,89 @@
             </div>
         </div>
     </section>
-    <section class="quote">
-        <div class="quote_container">
-            <div class="the_quote">
-                <h2>{{ ($quote[0]['q']) }}</h2>
-                <h3>{{ $quote[0]['a'] }}</h3>
+    <section class="quote_motivation">
+        <div class="quote">
+            <div class="quote_container">
+                <div class="the_quote">
+                    <h2>{{ ($quote[0]['q']) }}</h2>
+                    <h3>{{ $quote[0]['a'] }}</h3>
+                </div>
+            </div>
+            <div class="quote_image">
             </div>
         </div>
-        <div class="quote_image">
-
+        <div class="leaderboard">
+            {{-- <p>LeaderBoard</p> --}}
+            <div class="leaderview">
+                <p>LeaderBoard</p>
+                <div class="leader1">
+                    <div class="dp">
+                        <i id="medel" class="fas fa-medal" style="color: #ffd700"></i>
+                        <i class="fa-solid fa-circle-user" id="leaderDp"></i>
+                    </div>
+                    <div class="leaderDetails">
+                        <div id="name">Uday_Krishna_Pal</div>
+                        <i class="fa-solid fa-gem" id="badgeDp"></i>
+                        <div id="points">7000</div>
+                    </div>
+                </div>
+                <div class="leader1">
+                    <div class="dp">
+                        <i id="medel" class="fas fa-medal" style="color: #C0C0C0"></i>
+                        <i class="fa-solid fa-circle-user" id="leaderDp"></i>
+                    </div>
+                    <div class="leaderDetails">
+                        <div id="name">Subhankar_$inha </div>
+                        <i class="fa-solid fa-gem" id="badgeDp"></i>
+                        <div id="points">6890</div>
+                    </div>
+                </div>
+                <div class="leader1">
+                    <div class="dp">
+                        <i id="medel" class="fas fa-medal" style="color: #CD7F32"></i>
+                        <i class="fa-solid fa-circle-user" id="leaderDp"></i>
+                    </div>
+                    <div class="leaderDetails">
+                        <div id="name">Saikat_Dutta35 </div>
+                        <i class="fa-solid fa-star-half-stroke" id="badgeDp"></i>
+                        <div id="points">5742</div>
+                    </div>
+                </div>
+                <div class="leader1">
+                    <div class="dp">
+                        <div id="noId">4</div>
+                        <i class="fa-solid fa-circle-user" id="leaderDp"></i>
+                    </div>
+                    <div class="leaderDetails">
+                        <div id="name">Hacker_Win</div>
+                        <i class="fa-solid fa-shield-virus" id="badgeDp"></i>
+                        <div id="points">5342</div>
+                    </div>
+                </div>
+                <div class="leader1">
+                    <div class="dp">
+                        <div id="noId">5</div>
+                        <i class="fa-solid fa-circle-user" id="leaderDp"></i>
+                    </div>
+                    <div class="leaderDetails">
+                        <div id="name">Ronit$ingh</div>
+                        <i class="fa-solid fa-shield-cat" id="badgeDp"></i>
+                        <div id="points">5742</div>
+                    </div>
+                </div>
+                <div class="leader1">
+                    <div class="dp">
+                        <div id="noId">6</div>
+                        <i class="fa-solid fa-circle-user" id="leaderDp"></i>
+                    </div>
+                    <div class="leaderDetails">
+                        <div id="name">Pritam_Bag002</div>
+                        <i class="fa-solid fa-shield-heart" id="badgeDp"></i>
+                        <div id="points">5000</div>
+                    </div>
+                </div>
+            </div>
         </div>
-        
     </section>
     <footer class="footer">
         <div class="row1">
