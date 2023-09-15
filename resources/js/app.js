dd6001b0d698be9f38fcd8372d3083438755e4cc
PritamBag <pritam02b@gmail.com>
2023-09-15 17:16:14 +0530

    Edit modals are added for profile page

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,6 @@
 import "./bootstrap.js";
 
+//category.blade.php
 document.addEventListener("DOMContentLoaded", function () {
     let prevContent = null;
     function toggleDisplay(event) {
@@ -86,4 +87,91 @@ document.addEventListener("DOMContentLoaded", function () {
             contentDiv3.style.display = "none";
         });
     });
+
 });
+
+
+
+//profile.blade.php
+document.addEventListener("DOMContentLoaded", function () {
+    // Code for handling the Profile modal
+    const editProfileButton = document.getElementById("editProfileButton");
+    const editModal = document.getElementById("ProfileModal");
+    editProfileButton.addEventListener("click", () => {
+        editModal.style.display = "block";
+    });
+    const closeEditModalBtn = document.getElementById("closeEditModalBtn");
+    closeEditModalBtn.addEventListener("click", () => {
+        editModal.style.display = "none";
+    });
+    window.addEventListener("click", (event) => {
+        if (event.target === editModal) {
+            editModal.style.display = "none";
+        }
+    });
+
+    // Code for handling the Password modal
+    const editPasswordButton = document.getElementById("editPasswordButton");
+    const editPasswordModal = document.getElementById("PasswordModal");
+    editPasswordButton.addEventListener("click", () => {
+        editPasswordModal.style.display = "block";
+    });
+    const closePasswordBtn = document.getElementById("closePasswordBtn");
+    closePasswordBtn.addEventListener("click", () => {
+        editPasswordModal.style.display = "none";
+    });
+    window.addEventListener("click", (event) => {
+        if (event.target === editPasswordModal) {
+            editPasswordModal.style.display = "none";
+        }
+    });
+
+    // Code for handling the Education modal
+    const editEducationButton = document.getElementById("editEducationButton");
+    const editEducationModal = document.getElementById("EducationModal");
+    editEducationButton.addEventListener("click", () => {
+        editEducationModal.style.display = "block";
+    });
+    const closeEducationBtn = document.getElementById("closeEducationModalBtn");
+    closeEducationBtn.addEventListener("click", () => {
+        editEducationModal.style.display = "none";
+    });
+    window.addEventListener("click", (event) => {
+        if (event.target === editEducationModal) {
+            editEducationModal.style.display = "none";
+        }
+    });
+
+    // Code for handling the Bio modal
+    const editBioButton = document.getElementById("editBioButton");
+    const bioModal = document.getElementById("BioModal");
+    editBioButton.addEventListener("click", () => {
+        bioModal.style.display = "block";
+    });
+    const closeBioBtn = document.getElementById("closeBioModalBtn");
+    closeBioBtn.addEventListener("click", () => {
+        bioModal.style.display = "none";
+    });
+    window.addEventListener("click", (event) => {
+        if (event.target === bioModal) {
+            bioModal.style.display = "none";
+        }
+    });
+
+    // Code for handling the Social Media modal
+    const addSocialButton = document.getElementById("editSocialButton");
+    const socialModal = document.getElementById("SocialModal");
+    addSocialButton.addEventListener("click", () => {
+        socialModal.style.display = "block";
+    });
+    const closeSocialBtn = document.getElementById("closeSocialModalBtn");
+    closeSocialBtn.addEventListener("click", () => {
+        socialModal.style.display = "none";
+    });
+    window.addEventListener("click", (event) => {
+        if (event.target === socialModal) {
+            socialModal.style.display = "none";
+        }
+    });
+});
+
