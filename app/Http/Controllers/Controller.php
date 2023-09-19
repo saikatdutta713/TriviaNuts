4a042c23038c0f8a4df8bdffe47d493225bd5fa3
Saikat Dutta <saikatdutta713@gmail.com>
2023-09-19 23:57:59 +0530

    Index method for home view created

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,12 +7,21 @@ use Illuminate\Http\Request;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Support\Facades\Auth;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
 
-    function store(Request $request){
-        return User::find($request->id);
+    function index(){
+        return view('home');
+    }
+
+    function test(Request $request)
+    {
+        dd(User::where("verification_token", "y8U6WyiUzY3OLdk1YgC2ZLPxAx42dX9afNFEraPw")->update([
+            'email_verified_at' => now(),
+        ]));
+        // dd(now());
     }
 }
