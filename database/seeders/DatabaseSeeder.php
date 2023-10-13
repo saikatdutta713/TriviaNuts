f22b1837ef73c1e7defd33119c8039ebc667e6f3
PritamBag <pritam02b@gmail.com>
2023-10-13 15:18:11 +0530

    All seeders added as per priority for seeding

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,7 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +12,18 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        \App\Models\User::factory(10)->create();
+        $this->call([
+        UserSeeder::class,
+        CategorySeeder::class,
+        QuestionSeeder::class,
+        SetsSeeder::class,
+        QuizSeeder::class,
+        BadgesSeeder::class,
+        ScoreSeeder::class,
+        UserAnswersSeeder::class,
+        ParticipantsSeeder::class,
+        UserQuizProgressSeeder::class,
+        ]);     
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
     }
 }
