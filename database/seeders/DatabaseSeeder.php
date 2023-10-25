a0ce460901bcf599698cc3c31fd40ba06f047aa2
PritamBag <pritam02b@gmail.com>
2023-10-25 11:45:45 +0530

    Seeder for notification created

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -28,6 +28,7 @@ class DatabaseSeeder extends Seeder
             ForumAnswersSeeder::class,
             CommentsSeeder::class,
             ForumPostsSeeder::class,
+            NotificationsSeeder::class,
         ]);
     }
 }
